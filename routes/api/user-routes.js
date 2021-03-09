0d29eaff01120b3d3fe59398ff7689347875b720
Matt Gross <mbgross111@gmail.com>
2021-03-08 23:41:46 -0800

    add post route to /api/users/login to verify user credentials. add .checkPassword('textPass') method to User Model to compare hashed password with plain text

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -70,6 +70,28 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// Login route
+router.post('/login', (req, res) => {
+  // expects { email:'blah@blah.com', password: 'secret' }
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if(!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+    // verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return
+    }
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 // DELETE /api/users/id
 router.delete('/:id', (req, res) => {
   User.destroy({
